313f62608e74bd912aff561cf6dc6752bb244b8a
hsuan7752 <hsuan7752@gmail.com>
2021-12-09 15:24:25 +0800

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public GameObject tinyExplosionPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.transform.position = new Vector3(40.0f, 2.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            this.GetComponent<Rigidbody>().AddForce(Vector3.left * speed);
+
+        if (Input.GetKey(KeyCode.A))
+            this.GetComponent<Rigidbody>().AddForce(Vector3.back * speed);
+
+        if (Input.GetKey(KeyCode.D))
+            this.GetComponent<Rigidbody>().AddForce(Vector3.forward * speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 11)
+        {
+            this.gameObject.SetActive(false);
+            tinyExplosionPrefab.transform.position = this.transform.position;
+            Instantiate(tinyExplosionPrefab, transform.position, transform.rotation);
+            Destroy(tinyExplosionPrefab);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Kegel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Kegel : MonoBehaviour
+{
+    public GameObject[] kegel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = new Vector3(-12.0f, 0.5f, 0.0f);
+
+        kegel[0].transform.localPosition = new Vector3(4.0f, 0.0f, 0.0f);
+        kegel[1].transform.localPosition = new Vector3(2.0f, 0.0f, -2.0f);
+        kegel[2].transform.localPosition = new Vector3(2.0f, 0.0f, 2.0f);
+        kegel[3].transform.localPosition = new Vector3(0.0f, 0.0f, -4.0f);
+        kegel[4].transform.localPosition = new Vector3(0.0f, 0.0f, 4.0f);
+        kegel[5].transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+        kegel[6].transform.localPosition = new Vector3(-2.0f, 0.0f, -2.0f);
+        kegel[7].transform.localPosition = new Vector3(-2.0f, 0.0f, 2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
